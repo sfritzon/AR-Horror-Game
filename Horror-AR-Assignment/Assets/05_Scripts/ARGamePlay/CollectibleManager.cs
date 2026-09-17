dae073f2ac5c8ea1280d57d2a544aa26eec0e3d8
Jessicatran2004 <jessica.tran@student.forsbergsskola.se>
2026-09-17 15:16:34 +0200

    Add 3D jumpscare

---
+++ Horror-AR-Assignment/Assets/05_Scripts/ARGamePlay/CollectibleManager.cs
@@ -0,0 +1,84 @@
+using UnityEngine.InputSystem;
+using TMPro;
+using UnityEngine;
+
+public class CollectibleManager : MonoBehaviour
+{
+    [SerializeField] private TMP_Text collectibleText;
+    [SerializeField] private int totalCollectibles = 3;
+
+    [Header("Jumpscare")]
+    [SerializeField] private GameObject jumpScarePrefab;
+    [SerializeField] private float jumpScareDistance = 0.7f;
+
+    private int collectedCount = 0;
+
+    private void Start()
+    {
+        UpdateText();
+    }
+
+    public void CollectItem()
+    {
+        if (collectedCount >= totalCollectibles)
+            return;
+
+        collectedCount++;
+        UpdateText();
+
+        if (collectedCount == totalCollectibles)
+        {
+            ShowJumpScare();
+        }
+    }
+
+    private void UpdateText()
+    {
+        collectibleText.text =
+            $"Items found: {collectedCount}/{totalCollectibles}";
+    }
+
+    private void ShowJumpScare()
+    {
+        Camera arCamera = Camera.main;
+
+        if (arCamera == null || jumpScarePrefab == null)
+            return;
+
+        Vector3 spawnPosition =
+            arCamera.transform.position +
+            arCamera.transform.forward * jumpScareDistance;
+
+        GameObject ghost = Instantiate(
+            jumpScarePrefab,
+            spawnPosition,
+            Quaternion.identity);
+
+        // Make it smaller
+        ghost.transform.localScale = Vector3.one * 0.15f;
+
+        // Face the camera
+        ghost.transform.LookAt(arCamera.transform);
+
+        // Stop physics from moving it
+        Rigidbody rb = ghost.GetComponent<Rigidbody>();
+
+        if (rb != null)
+        {
+            rb.useGravity = false;
+            rb.isKinematic = true;
+            rb.linearVelocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+        }
+    }
+#if UNITY_EDITOR
+    private void Update()
+    {
+        if (Keyboard.current != null &&
+            Keyboard.current.tKey.wasPressedThisFrame)
+        {
+            CollectItem();
+        }
+    }
+#endif
+}
